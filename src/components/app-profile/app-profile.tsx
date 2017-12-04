9941610bd0e80f90e7a54ee9f25d32432f7d445e
Utwo <mihai.legat@gmail.com>
2017-12-04 17:08:45 +0200

    Add stencil

---
+++ src/components/app-profile/app-profile.tsx
@@ -0,0 +1,24 @@
+import { Component, Prop } from '@stencil/core';
+import { MatchResults } from '@stencil/router';
+
+
+@Component({
+  tag: 'app-profile',
+})
+export class AppProfile {
+
+  @Prop() match: MatchResults;
+
+  render() {
+    if (this.match && this.match.params.name) {
+      return (
+        <div>
+          <p>
+            Hello! My name is {this.match.params.name}.
+            My name was passed in through a route param!
+          </p>
+        </div>
+      );
+    }
+  }
+}
